71ce9111caddf4e20c164ed17503601a04117cba
JMajor99 <jdmajor99@gmail.com>
2026-02-14 11:46:48 +0700

    Update main.py

--- AI/main.py
+++ AI/main.py
@@ -2,3 +2,8 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+
+load_dotenv() # Load environment variables from .env file
+
+llm = ChatOpenAI()
+llm2 = ChatAnthropic()
